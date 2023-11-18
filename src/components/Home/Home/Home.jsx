48453d6f31d56d2d7278842b2bc3650fd00631a1
yeasin550 <yeasin51570@gmail.com>
2023-11-18 21:54:27 +0600

    per page some user info show and set up pagination

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,10 +1,9 @@
 import Users from "../../Users/Users";
-import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+           
             <Users/>
         </div>
     );
